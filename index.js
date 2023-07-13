7b543fce011fc1e73bcc5b9e2d1b9300e445a93a
robertnjunge <mubearobert@gmail.com>
2023-07-13 16:50:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+
+// // Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+
+function upperCaseCustomerName() {
+        customerName = customerName.toUpperCase();
+        return customerName;
+    }
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'Victor';
+    leastFavoriteCustomer = 'Joan';
+    return leastFavoriteCustomer;
+}
+
+
